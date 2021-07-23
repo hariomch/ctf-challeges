aa6efd1f3366e53b2e16cd3c53017a3424aba930
Hariom Chaturvedi <hariom4694@gmail.com>
2021-07-23 23:13:46 +0530

    Updated beep-boop
    Signed-off-by: Hariom Chaturvedi <hariom4694@gmail.com>

--- web/beep-boop/src/app/app.js
+++ web/beep-boop/src/app/app.js
@@ -7,6 +7,14 @@ router.get('/', function (req, res) {
   res.sendFile(path.join(__dirname + '/index.html'))
 })
 
+router.get('/robots.txt', function (req, res) {
+  res.sendFile(path.join(__dirname + '/robots.txt'))
+})
+
+router.get('/adminpage', function (req, res) {
+  res.sendFile(path.join(__dirname + '/adminpage.html'))
+})
+
 app.use('/', router)
 app.listen(process.env.port || 3000)
 
